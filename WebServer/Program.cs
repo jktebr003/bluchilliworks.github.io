aaecb496217c6e0d1ef261a02afcfc9a3938e160
Ebrahim Jakoet <38666402+jktebr003@users.noreply.github.com>
2026-03-06 00:51:00 +0200

    refactor: Clean up AlternateLayout and enhance routing with Fluxor integration

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,5 +1,15 @@
+using Blazor.SubtleCrypto;
+using Fluxor;
+using Fluxor.Blazor.Web.ReduxDevTools;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor.Extensions;
+using MudBlazor.Services;
+using Solutaris.InfoWARE.ProtectedBrowserStorage.Extensions;
 using WebServer.Components;
+using WebServer.Features.Authentication;
 using WebServer.Services;
+using WebServer.Shared;
+using WebServer.Shared.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +20,43 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<BusyDialogService>();
+
+builder.Services.AddIWProtectedBrowserStorage("WFCC7h70VDhZjS7AIJsGpvOGVoNNLp3aVM0OCNf8CSZQ78MphFCeNhf3XrxKLAnyO1iAWoBPJtUSIKsc"); //optionally pass an encryption key (as string)
+builder.Services.AddTransient<LocalStorageHelper>();
+
+builder.Services.AddHttpClient<WebApiClient>(client =>
+{
+    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+    client.DefaultRequestHeaders.AcceptLanguage.Clear();
+    //client.DefaultRequestHeaders.Add("Authorization", "4D2C6F5B-45CB-45B6-B5BA-7DC77EB22934");
+    client.DefaultRequestHeaders.Add("Authorization", builder.Configuration["AuthorizationKey"]);
+    //client.BaseAddress = new("https+http://apiservice");
+    client.BaseAddress = new(builder.Configuration["BaseApiServiceUrl"]);
+
+});
+
+// Fluxor (State Management)
+builder.Services.AddFluxor(options =>
+{
+    options.ScanAssemblies(typeof(Program).Assembly);
+    options.UseReduxDevTools();
+});
+
+builder.Services.AddSubtleCrypto(opt =>
+    opt.Key = "WFCC7h70VDhZjS7AIJsGpvOGVoNNLp3aVM0OCNf8CSZQ78MphFCeNhf3XrxKLAnyO1iAWoBPJtUSIKsc"
+);
+
+builder.Services.AddScoped<AuthenticationStateProvider, DatabaseAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<DeviceService>();
+builder.Services.AddScoped<GeoLocationService>();
+
+builder.Services.AddSingleton<BusyDialogService>();
+
+builder.Services.AddMudServices(options => { options.PopoverOptions.CheckForPopoverProvider = false; });
+builder.Services.AddMudExtensions();
+
 
 var app = builder.Build();
 
